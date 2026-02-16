4f501bceaf1c6a3ff3d27b19f0e2c7aedf19b85b
Tomás Hendrick Hernández Patiño <hendrickhk42@gmail.com>
2026-02-16 19:12:11 +0000

    Sitio Web Dinamico

--- web.py
+++ web.py
@@ -1,30 +1,35 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
 
+with open("home.html", encoding="utf-8") as f:
+    HOME = f.read()
+
+with open("1.html", encoding="utf-8") as f:
+    PROYECTO1 = f.read()
+
+contenido = {
+    "/": HOME,
+    "/proyecto/1": PROYECTO1,
+    "/proyecto/2": "<h1>Proyecto 2</h1>",
+    "/proyecto/3": "<h1>Proyecto 3</h1>",
+}
 
 class WebRequestHandler(BaseHTTPRequestHandler):
     def url(self):
         return urlparse(self.path)
 
-    def query_data(self):
-        return dict(parse_qsl(self.url().query))
+    # def query_data(self):
+    #     return dict(parse_qsl(self.url().query))
 
     def do_GET(self):
         path = self.url().path
 
-        if path == "/":
-            try:
-                with open("home.html", "r", encoding="utf-8") as f:
-                    content = f.read()
+        if path in contenido:
 
-                self.send_response(200)
-                self.send_header("Content-Type", "text/html; charset=utf-8")
-                self.end_headers()
-                self.wfile.write(content.encode("utf-8"))
-
-            except FileNotFoundError:
-                self.send_error(500, "home.html no encontrado")
-
+            self.send_response(200)
+            self.send_header("Content-Type", "text/html; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(contenido[path].encode("utf-8"))
         else:
             self.send_error(404, "Not Found")
 
